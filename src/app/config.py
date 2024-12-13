6075bdb7c3bb2af11336a48f5382f78984b918a0
Arun Shankar <arunprshankar@gmail.com>
2024-12-13 14:04:06 -0600

    wip

--- src/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///apis.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '../data')
